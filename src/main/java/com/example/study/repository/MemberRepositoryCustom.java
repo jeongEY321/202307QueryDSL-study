fb7628bb24171f1d82ad0e125960004629d274ba
jeongEY <kingship89@naver.com>
2023-07-11 14:23:22 +0900

    member - team / test

---
+++ src/main/java/com/example/study/repository/MemberRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.example.study.repository;
+
+import com.example.study.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    List<Member> findByName(String name);
+
+//    List<Member> sort(String 정렬조건);
+
+}
